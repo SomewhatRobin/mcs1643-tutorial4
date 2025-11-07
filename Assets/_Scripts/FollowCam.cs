8a9fc07b3d59efe22df80ac8aea99e576602469e
SomewhatRobin <jrobinso3@ltu.edu>
2025-11-07 18:45:57 -0500

    2 Videos In
    Yreah

--- Assets/_Scripts/FollowCam.cs
+++ Assets/_Scripts/FollowCam.cs
@@ -8,6 +8,8 @@ public class FollowCam : MonoBehaviour
     public float speed = 3.0f;
     public float zDistance = 10.0f;
     public float allowableOffset = 3.0f;
+    public Vector3 topLeft;
+    public Vector3 botRight;
     public GameObject player;
 
     // Improvements to consider:
@@ -29,5 +31,32 @@ public class FollowCam : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position + Vector3.back * zDistance, speed * Time.deltaTime);
         }
+
+        Vector3 pos = transform.position;
+
+        if (pos.x < topLeft.x)
+        { 
+            pos.x = topLeft.x;
+        }
+
+        if (pos.y > topLeft.y)
+        {
+            pos.y = topLeft.y;
+        }
+
+        if (pos.x > botRight.x)
+        {
+            pos.x = botRight.x;
+        }
+
+        if (pos.y < botRight.y)
+        {
+            pos.y = botRight.y;
+        }
+
+        pos.z = player.transform.position.z - zDistance;
+
+        transform.position = pos;
+
     }
 }
