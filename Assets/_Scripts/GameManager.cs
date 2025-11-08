79499295903ddabbd5c85dbf7b4d7f607fff20af
SomewhatRobin <jrobinso3@ltu.edu>
2025-11-07 21:19:31 -0500

    Vid3 13mins
    13:07 in

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject PauseMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseMenu.SetActive(false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu.SetActive(true);
+        }
+    }
+}
