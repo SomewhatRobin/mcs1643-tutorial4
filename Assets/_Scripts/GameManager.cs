2d7340cd5657306cc4d87d324d1a87f2137996de
SomewhatRobin <jrobinso3@ltu.edu>
2025-11-08 13:27:01 -0500

    Video 3 Done
    Added Main Menu, Added Continue Button+controls, Added Start Button, Added Menu Prefab

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public static bool _paused;
     public GameObject PauseMenu;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        PauseMenu.SetActive(false);    
+        PauseMenu.SetActive(false);
+        _paused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(!_paused)
         {
-            PauseMenu.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                PauseMenu.SetActive(true);
+                _paused = true;
+            }
         }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                PauseMenu.SetActive(false);
+                Time.timeScale = 1;
+                _paused = false;
+            }
+        }
+            
     }
 }
---
+++ Assets/_Scripts/MenuButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    //public string MainMenuSceneName = "MainMenu"
+    /*
+     ...
+    public void OnClick()
+    {
+        SceneManager.LoadScene(MainMenuSceneName);
+    }
+     
+     */
+
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
